2fb435b09adaf9c5a9d3ab89cad5749988ee7aa2
Palina Cetin <fpe@tut.by>
2020-10-02 15:51:42 +0300

    feat: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let expectedSymbolArray = [];
+    const BRACKETS = {
+      ')':'(', ']':'[', '}':'{', '2':'1', '4':'3', '6':'5'
+    }
+    for (let char of str) {        
+      if (char === '|' || char === '7' || char === '8') {
+        if (char === expectedSymbolArray[expectedSymbolArray.length-1] ) {
+          expectedSymbolArray.pop();        
+        } 
+        else {
+          expectedSymbolArray.push(char);
+          console.log(expectedSymbolArray);        
+        }; 
+      }
+      else if (char === '(' || char === '{'|| char === '[' || char === '1' || char === '3' || char === '5') {
+        expectedSymbolArray.push(char);
+        console.log(expectedSymbolArray);
+      }
+      else if (char === ')' || char === '}'|| char === ']' || char === '2' || char === '4'|| char === '6') {
+        if ( BRACKETS[char] === expectedSymbolArray.pop() ) {
+          console.log(expectedSymbolArray);              
+          continue;
+        }
+        else {
+          return false;
+        }
+      }
+      else {
+        console.log(char);
+        continue;
+      }
+    }    
+    if (expectedSymbolArray.length !== 0) return false;
+    else return true;  
+  }
